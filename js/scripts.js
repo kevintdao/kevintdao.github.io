489ac2c25f26ff3a3c90e6bff6b8a15fe4f6292d
Kevin Dao <kevin-dao@uiowa.edu>
2022-09-16 17:26:30 -0500

    Add active navbar item on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li a");
+console.log(sections);
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section, i) => {
+    const sectionTop = section.offsetTop;
+    if (i === sections.length - 1 && scrollY >= 2500) {
+      current = section.getAttribute("id");
+    }
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
 jQuery(function ($) {
-
-    'use strict';
-
-    // --------------------------------------------------------------------
-    // PreLoader
-    // --------------------------------------------------------------------
-
-    (function () {
-        $('#preloader').delay(200).fadeOut('slow');
-    }());
-
-
-
-    // --------------------------------------------------------------------
-    // Sticky Sidebar
-    // --------------------------------------------------------------------
-
-    $('.left-col-block, .right-col-block').theiaStickySidebar();
-
-}); // JQuery end
+  "use strict";
+  (function () {
+    $("#preloader").delay(200).fadeOut("slow");
+  })();
+});
