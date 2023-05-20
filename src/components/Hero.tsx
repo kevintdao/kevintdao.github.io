8834ec5b8ad9b8c8b5f3ec89d58b278352aaa5a6
Kevin Dao <kevin-dao@uiowa.edu>
2023-05-20 12:32:03 -0500

    Fix typo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ const Main = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 py-32">
         <div className="flex flex-col items-center">
           <p className="uppercase text-sm tracking-widest text-gray-600 py-2">
-            Welcome to my portfollio!
+            Welcome to my portfolio!
           </p>
           <img
             className="rounded-lg"
